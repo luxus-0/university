09cab3773d1adb2b5a82c5fa8d39c501f22a26d1
luxus-0 <luxus0@gmail.com>
2025-11-22 12:42:05 +0100

    Add annotation @EnableJpaAuditing

--- src/main/java/com/company/university/UniversityApplication.java
+++ src/main/java/com/company/university/UniversityApplication.java
@@ -2,8 +2,10 @@ package com.company.university;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UniversityApplication {
     public static void main(String[] args) {
         SpringApplication.run(UniversityApplication.class, args);
